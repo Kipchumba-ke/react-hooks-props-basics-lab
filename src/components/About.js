726000f7e24cf837236c3b789ff228b781ad425a
Kelvin Kipchumba <chogekelvin90@gmail.com>
2024-11-27 19:38:01 +0300

    props basics

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About({bio}) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio && bio.trim() !==`` && <p>{bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links/>
       {/* add your <Links /> component here */}
     </div>
   );
